ae697a34855d1be6c0dddd3b2a13d80362b2f3a3
kondrashovG <kondrashovg@gmail.com>
2023-03-26 23:27:11 +0300

    Initial commit

---
+++ app_accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from app_accounts.models import Fund, Account
+
+
+# Register your models here.
+class FundAdmin(admin.ModelAdmin):
+    list_display = ('name', 'account')
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('account',)
+
+
+admin.site.register(Fund, FundAdmin)
+admin.site.register(Account, AccountAdmin)
